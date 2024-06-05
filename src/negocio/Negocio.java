218ae0d82b091eb7f1ac29218a8ddfaf264d93c1
Aluno <Aluno@10.85.198.10>
2024-06-05 16:29:16 -0300

    Negocio

---
+++ src/negocio/Negocio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package negocio;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Negocio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s1 = "ACA";
+        String s2 = "BOU";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+    }
+    
+}
